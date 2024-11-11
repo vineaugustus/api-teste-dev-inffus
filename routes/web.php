7bf35eae6d78f15bb91fb4ab56ab0639781d8524
Vinicius <vinicius@radek.com.br>
2024-11-11 14:43:24 -0300

    rotas criadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CharacterController;
 
-Route::get('/', function () {
+    Route::get('/', function () {
     return view('welcome');
+    Route::get('/characters', [CharacterController::class, 'index']);
+
+
 });
